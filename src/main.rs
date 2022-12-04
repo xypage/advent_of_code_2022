4b9e27b9ab354bf0e5d0e989b07ccff8ba9d8008
Xavier <xypage@gmail.com>
2022-12-04 01:21:46 -0800

    Finished day 4

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,10 @@ use std::fs;
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 fn main() {
-    day3();
+    day4();
 }
 
 #[cfg(test)]
@@ -48,25 +49,43 @@ mod tests {
         let example: String = String::from("vJrwpWtwJgWrhcsFMMfFFhFp\njqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\nPmmdzqPrVvPwwTWBwg\nwMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\nttgJtRGJQctTZtZT\nCrZsJsPPZsGzwwsLwLmpwMDw");
         assert_eq!(70, day3::part_two(&example));
     }
+
+    #[test]
+    fn day4_part_one() {
+        let example: String = String::from("2-4,6-8\n2-3,4-5\n5-7,7-9\n2-8,3-7\n6-6,4-6\n2-6,4-8");
+        assert_eq!(2, day4::part_one(&example));
+    }
+    #[test]
+    fn day4_part_two() {
+        let example: String = String::from("2-4,6-8\n2-3,4-5\n5-7,7-9\n2-8,3-7\n6-6,4-6\n2-6,4-8");
+        assert_eq!(4, day4::part_two(&example));
+    }
 }
 
 fn day1() {
     let contents =
         fs::read_to_string("./input/day1").expect("Should have been able to read the file");
-    println!("{}", day1::part_one(&contents));
-    println!("{}", day1::part_two(&contents));
+    println!("Part one: {}", day1::part_one(&contents));
+    println!("Part two: {}", day1::part_two(&contents));
 }
 
 fn day2() {
     let contents =
         fs::read_to_string("./input/day2").expect("Should have been able to read the file");
-    println!("{}", day2::part_one(&contents));
-    println!("{}", day2::part_two(&contents));
+    println!("Part one: {}", day2::part_one(&contents));
+    println!("Part two: {}", day2::part_two(&contents));
 }
 
 fn day3() {
     let contents =
         fs::read_to_string("./input/day3").expect("Should have been able to read the file");
-    println!("{}", day3::part_one(&contents));
-    println!("{}", day3::part_two(&contents));
+    println!("Part one: {}", day3::part_one(&contents));
+    println!("Part two: {}", day3::part_two(&contents));
+}
+
+fn day4() {
+    let contents =
+        fs::read_to_string("./input/day4").expect("Should have been able to read the file");
+    println!("Part one: {}", day4::part_one(&contents));
+    println!("Part two: {}", day4::part_two(&contents));
 }
