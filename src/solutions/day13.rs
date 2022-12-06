41fea5d88bf4675312928065a75478d012b29752
xypage <xypage@gmail.com>
2022-12-05 20:34:14 -0800

    Filled in every day with a boilerplate file and added them to mod.rs to simplify new days

---
+++ src/solutions/day13.rs
@@ -0,0 +1,14 @@
+use crate::solutions::Solution;
+
+pub struct Day13;
+impl Solution for Day13 {
+    #[allow(unused_variables)]
+    fn part1(&self, _input: &String) -> String {
+        String::from("Part 1")
+    }
+
+    #[allow(unused_variables)]
+    fn part2(&self, _input: &String) -> String {
+        String::from("Part 2")
+    }
+}
---
+++ src/solutions/day17.rs
@@ -0,0 +1,14 @@
+use crate::solutions::Solution;
+
+pub struct Day17;
+impl Solution for Day17 {
+    #[allow(unused_variables)]
+    fn part1(&self, _input: &String) -> String {
+        String::from("Part 1")
+    }
+
+    #[allow(unused_variables)]
+    fn part2(&self, _input: &String) -> String {
+        String::from("Part 2")
+    }
+}
