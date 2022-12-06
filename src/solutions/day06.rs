983d9c20c200ba4578fed38bc21a3de24ba0dfb3
xypage <xypage@gmail.com>
2022-12-06 00:17:42 -0800

    Got an idea for a better solution from the discord and implemented it

--- src/solutions/day06.rs
+++ src/solutions/day06.rs
@@ -1,13 +1,13 @@
-use std::string::ToString;
 use std::collections::HashMap;
+use std::string::ToString;
 
 use crate::solutions::Solution;
 
 pub struct Day06;
 impl Solution for Day06 {
     fn part1(&self, input: &String) -> String {
-        // This is the original code I used, however the solution for part2 works for part1 if I just modify 
-        // the tracker size so I've changed it to just using that as well
+        // This is the original code I used, however my improved solutions work here as well if
+        // I just shift the numbers so I'm doing that here too
         // let iter = input.as_bytes().windows(4);
         // for (i, window) in iter.enumerate() {
         //     if window[0] != window[1] && window [0] != window [2] && window[0] != window[3] &&
@@ -20,33 +20,79 @@ impl Solution for Day06 {
 
         // // Random string to return if nothing else
         // String::from("No match, didn't work")
-        
-        // Here's the only difference from part2, I set the size to 4
-        let mut t = Tracker::new(4);
-        for char in input.as_bytes() {
-            t.insert(*char);
-            if t.check() {
-                return t.counter.to_string();
+
+        // Copied my second improved solution from part 2
+        const PACKET_SIZE: i32 = 4;
+
+        let mut last_seen: [usize; 26] = [0; 26];
+        let mut countdown: i32 = 0;
+        for (index, char) in input.as_bytes().iter().enumerate() {
+            let dist: i32 = (index - last_seen[(char - b'a') as usize]) as i32;
+            last_seen[(char - b'a') as usize] = index;
+            countdown -= 1;
+
+            if dist >= PACKET_SIZE && countdown < 1 {
+                return (index + 1).to_string();
+            }
+
+            if PACKET_SIZE - dist > countdown {
+                countdown = PACKET_SIZE - dist;
             }
         }
-        String::from("No match, didn't work")
+
+        String::from("No match")
     }
 
     fn part2(&self, input: &String) -> String {
-        // Make a blank tracker struct
-        let mut t = Tracker::new(14);
-        // turn it to bytes to iterate and store more easily, then go through all of them
-        for char in input.as_bytes() {
-            // push the current character
-            t.insert(*char);
-            // and checking till we get a positive, not technically necessary for the first 14 but also very easy
-            if t.check() {
-                // if it does tell us that we have a positive, we can use the tracker's counter to know what index that character is
-                return t.counter.to_string();
+        // // Make a blank tracker struct
+        // let mut t = Tracker::new(14);
+        // // turn it to bytes to iterate and store more easily, then go through all of them
+        // for char in input.as_bytes() {
+        //     // push the current character
+        //     t.insert(*char);
+        //     // and checking till we get a positive, not technically necessary for the first 14 but also very easy
+        //     if t.check() {
+        //         // if it does tell us that we have a positive, we can use the tracker's counter to know what index that character is
+        //         return t.counter.to_string();
+        //     }
+        // }
+        // // random string to return if nothing else
+        // String::from("No match, didn't work")
+
+        // The size of a packet, makes this a lot easier to use for part 1 or any other theoretical size that's similar
+        const PACKET_SIZE: i32 = 14;
+
+        // an array of the indices where a given character last showed up
+        let mut last_seen: [usize; 26] = [0; 26];
+        // A countdown for when the current nearest repeat drops out
+        //  For example, with a window of 4, if we had x(yzza)_bba then when you were at the cursor, you have a countdown of 2
+        //  since you need to move 2 for that old z to drop out, then you move forward and have xy(zzab)_ba and have
+        //  a countdown of 1 because you move 1 and if nothing else comes in you can send it, giving xyz(zabb)_a and now your
+        //  countdown is 3 since you need to move the b out, etc.
+        let mut countdown: i32 = 0;
+        for (index, char) in input.as_bytes().iter().enumerate() {
+            // Since we iterated forwards 1 we decrement the counter
+            countdown -= 1;
+
+            // get the distance between the index and the index of the last time you saw that character
+            let dist: i32 = (index - last_seen[(char - b'a') as usize]) as i32;
+            // since that's stored, we can write the current index to the "last seen" time for that character
+            last_seen[(char - b'a') as usize] = index;
+
+            // if the distance is larger than the packet size, and the countdown is < 1 (so we don't have any more to wait)
+            if dist >= PACKET_SIZE && countdown < 1 {
+                // then we got it, return that + 1 to 1-index it
+                return (index + 1).to_string();
+            }
+
+            // otherwise just update countdown if the new "time to wait till the duplicate drops off"
+            // is greater than the current one is
+            if PACKET_SIZE - dist > countdown {
+                countdown = PACKET_SIZE - dist;
             }
         }
-        // random string to return if nothing else
-        String::from("No match, didn't work")
+
+        String::from("No match")
     }
 }
 
@@ -54,7 +100,7 @@ struct Tracker {
     size: usize,
     list: Vec<u8>,
     map: HashMap<u8, u32>,
-    counter: u32
+    counter: u32,
 }
 
 impl Tracker {
@@ -64,7 +110,7 @@ impl Tracker {
             list: Vec::new(),
             // not strictly necessary but since we know at the start might as well
             map: HashMap::new(),
-            counter: 0
+            counter: 0,
         }
     }
 
@@ -92,8 +138,8 @@ impl Tracker {
         }
     }
 
-    fn check(&self) -> bool{
-        // length of the map is the number of elements which is going to be the number of elements in the 
+    fn check(&self) -> bool {
+        // length of the map is the number of elements which is going to be the number of elements in the
         // current window so when that's the same as the size, we know each element is unique otherwise
         // we'd have fewer keys since some would have a value of 2
         return self.map.len() == self.size;
