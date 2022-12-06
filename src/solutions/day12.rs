aefa666d1bc2bf2314d7041dba45f45e7e0f34eb
xypage <xypage@gmail.com>
2022-12-05 23:23:35 -0800

    Changed the boilerplate a little

--- src/solutions/day12.rs
+++ src/solutions/day12.rs
@@ -3,12 +3,16 @@ use crate::solutions::Solution;
 pub struct Day12;
 impl Solution for Day12 {
     #[allow(unused_variables)]
-    fn part1(&self, _input: &String) -> String {
+    fn part1(&self, input: &String) -> String {
+
+
         String::from("Part 1")
     }
 
     #[allow(unused_variables)]
-    fn part2(&self, _input: &String) -> String {
+    fn part2(&self, input: &String) -> String {
+
+
         String::from("Part 2")
     }
 }
